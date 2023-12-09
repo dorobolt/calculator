5e08959b2fb803873c375102a27c1da55798b5f0
dorobolt <91879792+dorobolt@users.noreply.github.com>
2023-12-09 17:05:58 +0700

    Finishing all calculator function

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ let opr = '';
 let opr2 = '';
 
 const display = document.querySelector('#display');
-const upDisplay = document.querySelector('#updisplay');
-display.textContent = '';
-upDisplay.textContent = '';
-const number = document.querySelectorAll('.number');
+display.textContent = ' ';
 
+const number = document.querySelectorAll('.number');
 for (let i = 0; i < number.length; i++) {
     number[i].addEventListener('click', () => {
         num1 += number[i].textContent;
@@ -19,22 +17,29 @@ for (let i = 0; i < number.length; i++) {
 const sum = document.querySelectorAll('.operator');
 for (let o = 0; o < sum.length; o++) {
     sum[o].addEventListener('click', () => {
-        // operator(sum[o].textContent);
-        if (num2 === '') {
+        if (num2 === '' && num1 !== '') {
+            enable();
             opr = sum[o].textContent;
-            upDisplay.textContent = num1 + ` ${opr}`;
+            sum[o].classList.add('pushed')
             opr2 = opr;
             num2 = num1
             num1 = '';
-            display.textContent = num1;
+            display.textContent = num2;
         }
+        else if (num1 === '' && num2 !== '') opr = sum[o].textContent;
         else {
             operator(opr);
+            enable();
             opr2 = opr;
             opr = sum[o].textContent;
-            upDisplay.textContent = num2 + ` ${opr}`;
+            display.textContent = num2;
+            sum[o].classList.add('pushed')
             num1 = '';
         }
+        console.log(num1);
+        console.log(num2);
+        console.log(opr);
+        console.log(opr2);
     })
 };
 
@@ -48,38 +53,72 @@ result.addEventListener('click', () => {
 
     };
     let sum = result[opr](parseFloat(num2), parseFloat(num1));
-    upDisplay.textContent = num2 + ' ' + opr + " " + num1 + ' ' + '=' + ' ' + sum;
+    let roundSum = sum.toFixed(5);
+    sum = Number(roundSum);
+    if (num1 === '0' && opr === '/') sum = 'ERROR';
+    if (num1 === '') sum = num2;
+    display.textContent = sum;
+    opr = '';
+    opr2 = '';
+    num1 = sum;
+    num2 = '';
+    enable();
+});
+
+const backspace = document.querySelector('#delete');
+backspace.addEventListener('click', () => {
+    num1 = num1.slice(0, -1);
+    display.textContent = num1;
+})
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
     opr = '';
     opr2 = '';
     num1 = '';
     num2 = '';
-    sum = 0;
-    display.textContent = '';
+    display.textContent = ' '
 })
 
+const dot = document.querySelector('#dot');
+dot.addEventListener('click', point)
+
 function operator(opr) {
+    let sum
     if (opr === '+') {
-        let sum = parseFloat(num1) + parseFloat(num2);
-        num1 = '';
-        num2 = sum;
-        return num1, num2;
+        sum = parseFloat(num1) + parseFloat(num2);
     }
     else if (opr === '-') {
-        let sum = parseFloat(num2) - parseFloat(num1);
-        num1 = '';
-        num2 = sum;
-        return num1, num2;
+        sum = parseFloat(num2) - parseFloat(num1);
     }
     else if (opr === '*') {
-        let sum = parseFloat(num2) * parseFloat(num1);
-        num1 = '';
-        num2 = sum;
-        return num1, num2;
+        sum = parseFloat(num2) * parseFloat(num1);
     }
     else if (opr === '/') {
-        let sum = parseFloat(num2) / parseFloat(num1);
-        num1 = '';
-        num2 = sum;
-        return num1, num2;
+        if (num1 === '0') return num2 = 'ERROR';
+        sum = parseFloat(num2) / parseFloat(num1);
+        let roundSum = sum.toFixed(5);
+        sum = Number(roundSum);
+    }
+    num1 = '';
+    num2 = sum;
+    return num1, num2;
+}
+
+function point() {
+    if (num1.indexOf('.') == -1) {
+        num1 += '.'
+        display.textContent = num1;
+        return num1;
+    }
+    else {
+        return;
+    }
+
+}
+
+function enable() {
+    for (let e = 0; e < sum.length; e++) {
+        sum[e].classList.remove('pushed')
     }
 }
